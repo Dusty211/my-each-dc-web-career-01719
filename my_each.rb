bd3170757965f0192c4ca60a224338a85c7ea8b7
Kyle Houghton <kylebenz@hotmail.com>
2018-12-27 19:59:00 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,5 +1,4 @@
 
-
 def my_each(array)
   i = 0
   while i < array.length do
@@ -7,13 +6,4 @@ def my_each(array)
     i += 1
   end
   array
-#  else
-#    array
-#  end
 end
-
-test_array = [0, 1, 2, 3, 4]
-
-my_each(test_array)
-
-puts test_array
