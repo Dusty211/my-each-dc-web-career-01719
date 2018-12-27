dde0c6283fb8266d8a837b9d0fb23944bb7bbfb6
Kyle Houghton <kylebenz@hotmail.com>
2018-12-27 19:57:46 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,16 +1,16 @@
 
 
 def my_each(array)
-  if block_given?
+#  if block_given?
     i = 0
     while i < array.length do
       yield array[i]
       i += 1
     end
   array
-  else
-    array
-  end
+#  else
+#    array
+#  end
 end
 
 test_array = [0, 1, 2, 3, 4]
